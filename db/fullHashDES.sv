73b4aeeb57f16bec05715656821e2a62658d30bf
AMazzarella96 <alemazzy96@hotmail.it>
2021-05-30 18:41:15 +0200

    Architectural Update

--- db/fullHashDES.sv
+++ db/fullHashDES.sv
@@ -28,16 +28,29 @@ reg [3 : 0] h [0 : 7];
 //circuit state
 reg[1 : 0] state;
 //message reduction to 6 bit
-reg [5 : 0] M6;
+wire [5 : 0] M6;
 //s-box input at last round
 wire [5 : 0] C6 [0 : 7];
 //to store h[i] values
 wire [3 : 0] H_main_w_o [0 : 7];
 reg [3 : 0] H_main [0 : 7];
+reg [3 : 0] H_last [0 : 7];
 
 reg [31 : 0] digest;
 
+wire compute_msg;
+wire init;
+wire compute_final_round;
+
+
+assign compute_msg = counter > 0 && state === 1 && M_valid;
+assign init = state === 0 && M_valid && counter === 0;
+assign compute_final_round = state === 1 && counter === 0; 
+
+
 //assign M6 = {M[3] ^ M[2],M[1],M[0],M[7],M[6],M[5] ^ M[4]};
+
+
 
 mainHashIteration main(
     .idx(M6),
@@ -64,9 +77,13 @@ hashRound_final hashRound_f(
     
 end*/
 
+//assign M6 = {M[3] ^ M[2], M[1],M[0],M[7],M[6],M[5] ^ M[4]};
+assign M6 = {M[5] ^ M[4], M[6], M[7], M[0], M[1], M[3] ^ M[2]};
+
 always @(posedge clk or negedge rst_n) begin
 	
     if (!rst_n) begin
+        
         counter <= 0;
         hash_ready <= 0;
         state <= 0;
@@ -78,22 +95,31 @@ always @(posedge clk or negedge rst_n) begin
         H_main[5] <= h5_value;
         H_main[6] <= h6_value;
         H_main[7] <= h7_value;
-    end else if(!M_valid && counter > 0) begin //Wait until next message block
-        #0;
-    end else if(state === 0 && M_valid && !counter) begin
-        counter <= C_in;
+
+    end else if(init) begin
+        counter <= C_in - 1;
         C <= C_in;
-        M6 <= {M[3] ^ M[2],M[1],M[0],M[7],M[6],M[5] ^ M[4]};
-    end else if(state === 0 && counter >= 1) begin
-        if(counter === 1)begin
-			state <= 1;
-		end
+        H_main <= H_main_w_o;
+        state <= 1;
+        hash_ready <= 0;
+        //M6 <= {M[3] ^ M[2],M[1],M[0],M[7],M[6],M[5] ^ M[4]};
+    end else if(compute_msg) begin
+        //if(counter === 1)begin
+        //    $display("STATE = 1");
+		//	state <= 1;
+		//end
 		counter <= counter - 1;
         H_main <= H_main_w_o;
-        M6 = {M[3] ^ M[2],M[1],M[0],M[7],M[6],M[5] ^ M[4]};
-    end else if(state === 1 && counter === 0) begin //test final round
+        //M6 <= {M[3] ^ M[2],M[1],M[0],M[7],M[6],M[5] ^ M[4]};
+        $display("Counter: %d | %b %b %b %b %b %b %b %b", counter, H_main[0], H_main[1], H_main[2], H_main[3], H_main[4], H_main[5], H_main[6], H_main[7]);
+    end else if(compute_final_round) begin //test final round
         counter <= 0;
-        state <= 2; //added state in which final round is gonna be computed
+        //state <= 2; //added state in which final round is gonna be computed
+        //H_last <= H_main;
+        hash_ready <= 1; //final round computed 
+        state <= 0;
+        counter <= 0;
+        digest_final <= digest;
         H_main[0] <= h0_value;
         H_main[1] <= h1_value;
         H_main[2] <= h2_value;
@@ -102,12 +128,22 @@ always @(posedge clk or negedge rst_n) begin
         H_main[5] <= h5_value;
         H_main[6] <= h6_value;
         H_main[7] <= h7_value;
-    end else if (state === 2) begin 
-         hash_ready <= 1; //final round computed 
-         state <= 0;
-         counter <= 0;
-         digest_final <= digest;
-    end 
+    end /*else if (state === 2) begin
+        hash_ready <= 1; //final round computed 
+        state <= 0;
+        counter <= 0;
+        digest_final <= digest;
+        H_main[0] <= h0_value;
+        H_main[1] <= h1_value;
+        H_main[2] <= h2_value;
+        H_main[3] <= h3_value;
+        H_main[4] <= h4_value;
+        H_main[5] <= h5_value;
+        H_main[6] <= h6_value;
+        H_main[7] <= h7_value;
+    end */else if(!M_valid) begin //Wait until next message block
+        #0;
+    end
  end
 
 
@@ -129,6 +165,7 @@ module Sbox (
     always @(*) begin
           row = {in[5], in[0]};
           colum = in[4 : 1];
+          $display("M6: %b - COLUMN: %b", in, colum);
         /*assign row = {in[5], in[0]}
         assign colum = in[4 : 1 ];*/
         case (colum)
@@ -256,22 +293,22 @@ Sbox sbox (
         h_out[1] = tmp;
         //h[2]
         tmp = h[3] ^ s_value;
-        h_out[2] = tmp << 1 | tmp >> 3;
+        h_out[2] = (tmp << 1) | (tmp >> 3);
         //3
         tmp = h[4] ^ s_value;
-        h_out[3] = tmp << 1 | tmp >> 3;
+        h_out[3] = (tmp << 1) | (tmp >> 3);
         //4
         tmp = h[5] ^ s_value;
-        h_out[4] = tmp << 2 | tmp >> 2;
+        h_out[4] = (tmp << 2) | (tmp >> 2);
         //5
         tmp = h[6] ^ s_value;
-        h_out[5] = tmp << 2 | tmp >> 2;
+        h_out[5] = (tmp << 2) | (tmp >> 2);
         //6
         tmp = h[7] ^ s_value;
-        h_out[6] = tmp << 3 | tmp >> 1;
+        h_out[6] = (tmp << 3) | (tmp >> 1);
         //7      
         tmp = h[0] ^ s_value;
-        h_out[7] = tmp << 3 | tmp >> 1;
+        h_out[7] = (tmp << 3) | (tmp >> 1);
         
     end
 endmodule
@@ -283,59 +320,113 @@ module hashRound_final (
     input [3 : 0] h [0 : 7],
     output reg [31 : 0] digest //8 signal of 4 bits
 );
-reg [3 : 0] s_value;
+reg [3 : 0] s_value [0 : 7];
 reg [7 : 0] Ci;
-reg [5 : 0] idx;
+reg [5 : 0] idx [0 : 7];
 reg [3 : 0] tmp;
 reg [3 : 0] h_out [0 : 7];
-Sbox sbox (
-    .in (idx),
-    .out(s_value)
+
+Sbox sbox1 (
+    .in (idx[0]),
+    .out(s_value[0])
 );
+
+Sbox sbox2 (
+    .in (idx[1]),
+    .out(s_value[1])
+);
+
+Sbox sbox3 (
+    .in (idx[2]),
+    .out(s_value[2])
+);
+
+Sbox sbox4 (
+    .in (idx[3]),
+    .out(s_value[3])
+);
+
+Sbox sbox5 (
+    .in (idx[4]),
+    .out(s_value[4])
+);
+
+Sbox sbox6 (
+    .in (idx[5]),
+    .out(s_value[5])
+);
+
+Sbox sbox7 (
+    .in (idx[6]),
+    .out(s_value[6])
+);
+
+Sbox sbox8 (
+    .in (idx[7]),
+    .out(s_value[7])
+);
+
     always @(*) begin
         //h[0]
         Ci = C[7 : 0];
-        idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
-        tmp = h[1] ^ s_value;
+        //idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
+        idx[0] = {Ci[6], Ci[4], Ci[5] ^ Ci[0], Ci[2], Ci[3], Ci[7] ^ Ci[1]};
+        tmp = h[1] ^ s_value[0];
         h_out[0] = tmp;
+        $display("idx: %b | Sval: %b", idx[0], s_value[0]);
         //h[1]
         Ci = C[15 : 8];
-        idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
-        tmp = h[2] ^ s_value;
+        //idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
+        idx[1] = {Ci[6], Ci[4], Ci[5] ^ Ci[0], Ci[2], Ci[3], Ci[7] ^ Ci[1]};
+        tmp = h[2] ^ s_value[1];
         h_out[1] = tmp;
+        $display("idx: %b | Sval: %b", idx[1], s_value[1]);
         //h[2]
         Ci = C[23 : 16];
-        idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
-        tmp = h[3] ^ s_value;
-        h_out[2] = tmp << 1 | tmp >> 3;
+        //idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
+        idx[2] = {Ci[6], Ci[4], Ci[5] ^ Ci[0], Ci[2], Ci[3], Ci[7] ^ Ci[1]};
+        tmp = h[3] ^ s_value[2];
+        h_out[2] = (tmp << 1) | (tmp >> 3);
+        $display("idx: %b | Sval: %b", idx[2], s_value[2]);
         //3
         Ci = C[31 : 24];
-        idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
-        tmp = h[4] ^ s_value;
-        h_out[3] = tmp << 1 | tmp >> 3;
+        //idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
+        idx[3] = {Ci[6], Ci[4], Ci[5] ^ Ci[0], Ci[2], Ci[3], Ci[7] ^ Ci[1]};
+        tmp = h[4] ^ s_value[3];
+        h_out[3] = (tmp << 1) | (tmp >> 3);
+        $display("idx: %b | Sval: %b", idx[3], s_value[3]);
         //4
         Ci = C[39 : 32];
-        idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
-        tmp = h[5] ^ s_value;
-        h_out[4] = tmp << 2 | tmp >> 2;
+        //idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
+        idx[4] = {Ci[6], Ci[4], Ci[5] ^ Ci[0], Ci[2], Ci[3], Ci[7] ^ Ci[1]};
+        tmp = h[5] ^ s_value[4];
+        h_out[4] = (tmp << 2) | (tmp >> 2);
+        $display("idx: %b | Sval: %b", idx[4], s_value[4]);
         //5
         Ci = C[47 : 40];
-        idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
-        tmp = h[6] ^ s_value;
-        h_out[5] = tmp << 2 | tmp >> 2;
+        //idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
+        idx[5] = {Ci[6], Ci[4], Ci[5] ^ Ci[0], Ci[2], Ci[3], Ci[7] ^ Ci[1]};
+        tmp = h[6] ^ s_value[5];
+        h_out[5] = (tmp << 2) | (tmp >> 2);
+        $display("idx: %b | Sval: %b", idx[5], s_value[5]);
         //6
         Ci = C[55 : 48];
-        idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
-        tmp = h[7] ^ s_value;
-        h_out[6] = tmp << 3 | tmp >> 1;
+        //idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
+        idx[6] = {Ci[6], Ci[4], Ci[5] ^ Ci[0], Ci[2], Ci[3], Ci[7] ^ Ci[1]};
+        tmp = h[7] ^ s_value[6];
+        h_out[6] = (tmp << 3) | (tmp >> 1);
+        $display("idx: %b | Sval: %b",  idx[6], s_value[6]);
         //7 
         Ci = C[63 : 56];
-        idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};     
-        tmp = h[0] ^ s_value;
-        h_out[7] = tmp << 3 | tmp >> 1;
+        //idx = {Ci[7] ^ Ci[1], Ci[3], Ci[2], Ci[5] ^ Ci[0], Ci[4], Ci[6]};
+        idx[7] = {Ci[6], Ci[4], Ci[5] ^ Ci[0], Ci[2], Ci[3], Ci[7] ^ Ci[1]};     
+        tmp = h[0] ^ s_value[7];
+        h_out[7] = (tmp << 3) | (tmp >> 1);
+        $display("idx: %b | Sval: %b", idx[7], s_value[7]);
+        digest = {h_out[0], h_out[1], h_out[2], h_out[3], h_out[4], h_out[5], h_out[6], h_out[7]};
 
-        digest = {h_out[0], h_out[1], h_out[2], h_out[3], h_out[4], h_out[5], h_out[6]};
     end
+
 
 endmodule
 
@@ -345,7 +436,8 @@ module mainHashIteration (
     output [3 : 0] h_out [0 : 7]
 );
 
-wire [3 : 0] h1_in [0 : 7];
+
+
 wire [3 : 0] h1_out [0 : 7];
 hashRound round1 (
     .idx(idx),
--- tb/TestHash.sv
+++ tb/TestHash.sv
@@ -13,6 +13,7 @@ reg hash_ready;
 reg [63 : 0] C_in;
 reg [7 : 0] M;
 reg [31 : 0] digest;
+reg [7 : 0] txt [0 : 27];
 
 fullHashDES test_hash (
      .clk           (clk)
@@ -21,7 +22,7 @@ fullHashDES test_hash (
     ,.C_in 			(C_in)  
     ,.M 			(M)
     ,.hash_ready 	(hash_ready)
-    ,.digest 		(digest)
+    ,.digest_final 		(digest)
   );
   
 
@@ -36,7 +37,37 @@ end
 
 initial begin
 
-C_in = 64'd50;
+C_in = 64'd28;
+
+txt[0] = 8'b01001101;
+txt[1] = 8'b01100101;
+txt[2] = 8'b01110011;
+txt[3] = 8'b01110011;
+txt[4] = 8'b01100001;
+txt[5] = 8'b01100111;
+txt[6] = 8'b01100111;
+txt[7] = 8'b01101001;
+txt[8] = 8'b01101111;
+txt[9] = 8'b00100000;
+txt[10] = 8'b01101001;
+txt[11] = 8'b01101110;
+txt[12] = 8'b00100000;
+txt[13] = 8'b01100011;
+txt[14] = 8'b01101000;
+txt[15] = 8'b01101001;
+txt[16] = 8'b01100001;
+txt[17] = 8'b01110010;
+txt[18] = 8'b01101111;
+txt[19] = 8'b00100000;
+txt[20] = 8'b01100100;
+txt[21] = 8'b01101001;
+txt[22] = 8'b00100000;
+txt[23] = 8'b01110000;
+txt[24] = 8'b01110010;
+txt[25] = 8'b01101111;
+txt[26] = 8'b01110110;
+txt[27] = 8'b01100001;
+
 
 fork
 
@@ -48,14 +79,18 @@ fork
 		
 		for(i=0;i<C_in;i++)begin
 			M_valid = 1'b1;
-			M = i;
+			M = txt[i];
 			@(posedge clk);
-			M_valid = 1'b0;
-			@(posedge clk);
+			//M_valid = 1'b0;
+			//@(posedge clk);
 		end
-		
+
+
+		M_valid = 1'b0;
+
 		@(posedge clk);
-		
+		@(posedge clk);
+
 		if(hash_ready)begin
 			$display("Digest: %b", digest);
 		end
@@ -72,5 +107,59 @@ end
 endmodule
 // -----------------------------------------------------------------------------
 
-//for # Digest: 00000111000010001100100010000000	Digest: 0708c880 deciamle 118016128
-// 				00000011000010001100100010000000	Digest: 0308c880 decimale 50907264 
+// 1001 0101 1100 0101 1000 0111 1101 0111
+
+// 1111 0110 0101 1100 1110 1011 0100 0000
+
+// 0010 0110 0101 1100 1110 1011 0100 0000
+
+
+
+/*module Test;
+	
+	reg [3 : 0] H_main [0 : 7];
+	reg [31 : 0] digest;
+	reg [63 : 0] C;
+
+	localparam h0_value = 4'h4;
+	localparam h1_value = 4'hB;
+	localparam h2_value = 4'h7;
+	localparam h3_value = 4'h1;
+	localparam h4_value = 4'hD;
+	localparam h5_value = 4'hF;
+	localparam h6_value = 4'h0;
+	localparam h7_value = 4'h3;
+
+	hashRound_final h_final(
+		.C (C),
+    	.h(H_main),
+    	.digest(digest)
+	);
+
+	
+	initial begin
+
+
+	fork
+
+		begin: TEST1
+			C = 64'd0;
+			H_main[0] = h0_value;
+			H_main[1] = h1_value;
+			H_main[2] = h2_value;
+			H_main[3] = h3_value;
+			H_main[4] = h4_value;
+			H_main[5] = h5_value;
+			H_main[6] = h6_value;
+			H_main[7] = h7_value;		
+			$display("digest: %b", digest);
+
+
+		end:TEST1
+	join
+
+	end
+
+
+endmodule*/
+
